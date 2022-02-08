69b99d183a8a12307f9f48e7109b5e8114f107b5
Sami Shanagher <sshan90@hotmail.com>
2022-02-08 16:53:38 +0000

    added styling for the items but it is hideous and basic

--- db.php
+++ db.php
@@ -11,52 +11,53 @@ function getItems(object $db): array {
 function arraySplit(array $items) : string{
     $result = '';
     if (!count($items)) {
-        $result .= '<div>' . 'there aren\'t any database items to present' . '<div>';
+        $result .= '<div class = "empty">' . 'there aren\'t any database items to present' . '</div>';
     } else {
         foreach ($items as $item) {
+            $result .= '<div class="item">';
             if (!isset($item['image'])){
-                $result .= '<div>' . 'no image' . '<div>';
+                $result .= '<div class = "empty">' . 'no image' . '</div>';
             } else {
                 if (!$item['image']) {
-                    $result .= '<div>' . 'there is no image to present at this time' . '<div>';
+                    $result .= '<div> class = "empty"' . 'there is no image to present at this time' . '</div>';
                 } else {
-                    $result .= '<div><img src="'. $item['image'] . '" alt="garment image" width="500" height="500"></div>';
+                    $result .= '<div class = "image_wrap"><img class= "image" src="'. $item['image'] . '" alt="garment image"></div>';
                 }
             }
             if (!isset($item['name'])){
-                $result .= '<div>' . 'no name' . '<div>';
+                $result .= '<div class = "empty">' . 'no name' . '</div>';
             } else {
                 if (!$item['name']) {
-                    $result .= '<div>' . 'there isn\'t a name for this item' . '<div>';
+                    $result .= '<div class = "empty">' . 'there isn\'t a name for this item' . '</div>';
                 } else {
-                    $result .= '<div>' . $item['name'] . '</div>';
+                    $result .= '<div class = "name">' . $item['name'] . '</div>';
                 }
             }
             if (!isset($item['designer'])){
-                $result .= '<div>' . 'no designer' . '<div>';
+                $result .= '<div class = "empty">' . 'no designer' . '</div>';
             } else {
                 if (!$item['designer']) {
-                    $result .= '<div>' . 'the designer of this garment is not yet known' . '<div>';
+                    $result .= '<div class = "empty">' . 'the designer of this garment is not yet known' . '</div>';
                 } else {
-                    $result .= '<div>' . $item['designer'] . '</div>';
+                    $result .= '<div class="designer">' . $item['designer'] . '</div>';
                 }
             }
             if (!isset($item['style'])){
-                $result .= '<div>' . 'no style' . '<div>';
+                $result .= '<div class = "empty">' . 'no style' . '</div>';
             } else {
                 if (!$item['style']) {
-                    $result .= '<div>' . 'the style of this garment is not yet known' . '<div>';
+                    $result .= '<div class = "empty">' . 'the style of this garment is not yet known' . '</div>';
                 } else {
-                    $result .= '<div>' . $item['style'] . '</div>';
+                    $result .= '<div class = "style">' . $item['style'] . '</div>';
                 }
             }
             if (!isset($item['year_released'])){
-                $result .= '<div>' . 'no year' . '<div>';
+                $result .= '<div class = "empty">' . 'no year' . '</div>';
             } else {
                 if($item['year_released'] <= date("Y")) {
-                    $result .= '<div>' . $item['year_released'] . '</div>';
+                    $result .= '<div class="year">' . $item['year_released'] . '</div>' . '</div>';
                 } else {
-                    $result .= '<div>' . 'select a year that isn\'t in the future' . '<div>';
+                    $result .= '<div class = "empty">' . 'select a year that isn\'t in the future' . '</div>';
                 }
             }
         }
