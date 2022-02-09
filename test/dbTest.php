4e6882ffe6cbd82b10a45e9ed63ceb4620cc4f3c
Sami Shanagher <sshan90@hotmail.com>
2022-02-09 09:51:52 +0000

    Updated the Test with updated outputs from the function

--- test/dbTest.php
+++ test/dbTest.php
@@ -4,14 +4,14 @@ use PHPUnit\Framework\TestCase;
 class dbTest extends TestCase {
     public function testSuccessLoop(){
         $array = [['name' => 'simon', 'designer' => 'versace', 'style' => 'bomber', 'year_released' => 2000, 'image' => 'image.jpg']];
-        $expected = '<div><img src="image.jpg" alt="garment image" width="500" height="500"></div><div>simon</div><div>versace</div><div>bomber</div><div>2000</div>';
+        $expected = '<div class="item"><div class = "image_wrap"><img class= "image" src="image.jpg" alt="garment image"></div><div class = "name">simon</div><div class="designer">versace</div><div class = "style">bomber</div><div class="year">2000</div></div>';
         $inputA = $array;
         $case = arraySplit($inputA);
         $this->assertEquals($expected, $case);
     }
     public function testFailureLoop(){
         $array = [['name' => '', 'designer' => '', 'style' => '', 'year_released' => 2035, 'image' => '']];
-        $expected = '<div>there is no image to present at this time<div><div>there isn\'t a name for this item<div><div>the designer of this garment is not yet known<div><div>the style of this garment is not yet known<div><div>select a year that isn\'t in the future<div>'
+        $expected = '<div class="item"><div> class = "empty"there is no image to present at this time</div><div class = "empty">there isn\'t a name for this item</div><div class = "empty">the designer of this garment is not yet known</div><div class = "empty">the style of this garment is not yet known</div><div class = "empty">select a year that isn\'t in the future</div>'
         ;
         $inputA = $array;
         $case = arraySplit($inputA);
@@ -25,7 +25,7 @@ class dbTest extends TestCase {
     }
     public function testNoContent(){
         $array = [];
-        $expected = '<div>there aren\'t any database items to present<div>';
+        $expected = '<div class = "empty">there aren\'t any database items to present</div>';
         $case = arraySplit($array);
         $this->assertEquals($expected, $case);
     }
